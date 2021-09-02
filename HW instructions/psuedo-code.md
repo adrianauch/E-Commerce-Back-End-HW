1034c78710fcdb5df3312b8b7cff0e7520fef4fe
Adrian <adrian.auch11@gmail.com>
2021-09-02 17:55:10 -0500

    updated models, installed everything updated configuration

---
+++ HW instructions/psuedo-code.md
@@ -0,0 +1,9 @@
+What do I need to do to set up application?
+
+1. create an git ignore file
+2. .env file with proper set up inside
+3. I need to gitignore .env, dsstore and node modules
+4. npm i dotenv, mysql2.
+5. look at readme for how to set up models
+6. need to create associations
+7. NEED FORIEGN KEY.
--- models/index.js
+++ models/index.js
@@ -1,16 +1,31 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "Category_id",
+  onDelete: "CASCADE",
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "product_id",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(ProductTag, {
+  foreignKey: "tag_id",
+  onDelete: "CASCADE",
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(ProductTag, {
+  foreignKey: "product_id",
+  onDelete: "CASCADE",
+});
 
 module.exports = {
   Product,
